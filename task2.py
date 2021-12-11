2f84e0ef4ffca1cc3def52cef8b1a8fb84484510
Aleksey Miheev <aleksvalmix@yandex.ru>
2021-12-11 17:31:02 +0500

    fix

--- task2.py
+++ task2.py
@@ -1,7 +1,6 @@
 cubes = []
-for i in range(1, 1001):
-    if(i%2 != 0):
-        cubes.append(i*i*i)
+for i in range(1, 1001, 2):
+    cubes.append(i*i*i)
 
 # задача а
 cube_sum = 0
@@ -18,11 +17,13 @@ print("Задача а, сумма = " + str(cube_sum))
 # задача b-c
 cube_sum = 0
 for cube in cubes:
-    string = str(cube + 17)
+    cube += 17 # Павильная строка
+    string = str(cube)
+    # string = str(cube + 17) # Можете подсказать почему из за такого подхода ответ был 15392908808 а не 15392909930 как у всех
     sum = 0
     for number in string:
         sum += int(number)
-    if(sum % 7 == 0 and sum > 0):
+    if(sum % 7 == 0):
         cube_sum += cube
 
-print("Задача b-c, сумма = " + str(cube_sum))
+print("Задача b-c, сумма = ",cube_sum)
