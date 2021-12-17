6703c8089a9b8561373f40733ea89905d48600ad
Aleksey Miheev <aleksvalmix@yandex.ru>
2021-12-17 12:11:59 +0500

    fix

--- Mikheev_Aleksey_dz_2/task_2_5.py
+++ Mikheev_Aleksey_dz_2/task_2_5.py
@@ -25,6 +25,11 @@ list_id = id(list)
 print('id списка', list_id)
 list.sort()
 print(list)
+for good in list:
+    rub = int(good)
+    kk = (good - rub) * 100
+    print(f'{rub} руб {kk:02.0f} коп', end=', ') 
+
 print('id оригинала:', list_id, 'id отсортиранного списка:',
       id(list), 'Списки равны:', list_id == id(list))
 print()
@@ -33,9 +38,18 @@ print()
 print('Задание C')
 print(list)
 new_list = sorted(list, reverse=True)
-print(new_list)
+for good in new_list:
+    rub = int(good)
+    kk = (good - rub) * 100
+    print(f'{rub} руб {kk:02.0f} коп', end=', ') 
 print()
 
 # Задание D
 print('Задание D')
-print('Пять самых дорогих товаров:', new_list[0:5])
+list = [57.8, 46.51, 97, 13, 22.54, 95.12, 102, 99, 85, 43, 45.56, 205.01]
+list = sorted(list, reverse=True)
+for good in sorted(list, reverse=True)[:5]:
+    rub = int(good)
+    kk = (good - int(good)) * 100
+    print(f'{rub} руб {kk:02.0f} коп', end=', ')  # 02 - добавить поле нулей 2шт,  .0f - округление 0 знаков после запятой
+
