1233b1c81e0aa140bbbcfb655eea93011d49090d
Aleksey <aleksvalmix@yandex.ru>
2021-12-23 15:14:14 +0500

    lesson_4

---
+++ Mikheev_Aleksey_dz_4/task_4_3.py
@@ -0,0 +1,36 @@
+#*(вместо 2) Доработать функцию currency_rates(): теперь она должна возвращать кроме курса дату, которая передаётся в ответе сервера. Дата должна быть в виде объекта date. Подумайте, как извлечь дату из ответа, какой тип данных лучше использовать в ответе функции?
+
+
+#Написать функцию currency_rates(), принимающую в качестве аргумента код валюты (например, USD, EUR, GBP, ...) и возвращающую курс этой валюты по отношению к рублю. Использовать библиотеку requests. В качестве API можно использовать http://www.cbr.ru/scripts/XML_daily.asp. Рекомендация: выполнить предварительно запрос к API в обычном браузере, посмотреть содержимое ответа. Можно ли, используя только методы класса str, решить поставленную задачу? Функция должна возвращать результат числового типа, например float. Подумайте: есть ли смысл для работы с денежными величинами использовать вместо float тип Decimal? Сильно ли усложняется код функции при этом? Если в качестве аргумента передали код валюты, которого нет в ответе, вернуть None. Можно ли сделать работу функции не зависящей от того, в каком регистре был передан аргумент? В качестве примера выведите курсы доллара и евро.
+
+import requests;
+from decimal import *
+from datetime import datetime
+getcontext().prec = 4
+
+def currency_rates(code):
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
+
+
+    code = code.upper()
+
+    if code not in response:
+        return None
+
+    day_raw = response[response.find('Date="') + 6:response.find('"', response.find('Date="') + 6)].split('.')
+    day, month, year = map(int, day_raw)
+
+
+    response = response[response.find(code):]
+    response = response[:response.find('</Value>')]
+    rub = response[response.find('<Value>') + 7:]
+
+
+    return f"{Decimal(rub.replace(',', '.'))}, {datetime(day=day, month=month, year=year)}"
+
+
+
+
+
+print(currency_rates('USD'))
+
